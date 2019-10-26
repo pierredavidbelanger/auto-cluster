488795d9fa072204a8bb0b05162b3518708b70c9
Pierre-David Bélanger <pierredavidbelanger@gmail.com>
2019-10-25 21:56:18 -0400

    Update cloud-init.sh

--- cloud-init.sh
+++ cloud-init.sh
@@ -19,6 +19,7 @@ apt-get install -y awscli \
 instance_id=$(curl -fs http://169.254.169.254/latest/meta-data/instance-id)
 region=$(curl -fs http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 public_ip=$(curl -fs http://169.254.169.254/latest/meta-data/public-ipv4)
+public_hostname=$(curl -fs http://169.254.169.254/latest/meta-data/public-hostname)
 private_ip=$(curl -fs http://169.254.169.254/latest/meta-data/local-ipv4)
 private_hostname=$(curl -fs http://169.254.169.254/latest/meta-data/local-hostname)
 
@@ -28,14 +29,17 @@ private_hostname=$(curl -fs http://169.254.169.254/latest/meta-data/local-hostna
 asg_name=$(aws autoscaling describe-auto-scaling-instances --region "$region" --instance-ids "$instance_id" --query 'AutoScalingInstances[].AutoScalingGroupName' --output text)
 cluster_tag=""
 role_tag=""
+zone_tag=""
 
 if [ "$asg_name" != "" ]; then
   cluster_tag=$(aws autoscaling describe-tags --region "$region" --filters "Name=auto-scaling-group,Values=$asg_name" 'Name=key,Values=cluster' --query 'Tags[].Value' --output text)
   role_tag=$(aws autoscaling describe-tags --region "$region" --filters "Name=auto-scaling-group,Values=$asg_name" 'Name=key,Values=role' --query 'Tags[].Value' --output text)
+  zone_tag=$(aws autoscaling describe-tags --region "$region" --filters "Name=auto-scaling-group,Values=$asg_name" 'Name=key,Values=zone' --query 'Tags[].Value' --output text)
 else
   asg_name='none'
   cluster_tag=$(aws ec2 describe-tags --region "$region" --filters "Name=resource-id,Values=$instance_id" 'Name=key,Values=cluster' --query 'Tags[].Value' --output text)
   role_tag=$(aws ec2 describe-tags --region "$region" --filters "Name=resource-id,Values=$instance_id" 'Name=key,Values=role' --query 'Tags[].Value' --output text)
+  zone_tag=$(aws ec2 describe-tags --region "$region" --filters "Name=resource-id,Values=$instance_id" 'Name=key,Values=zone' --query 'Tags[].Value' --output text)
 fi
 
 if [ "$cluster_tag" == '' ]; then
@@ -55,6 +59,8 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
 
+usermod -aG docker ubuntu
+
 mkdir -p /etc/systemd/system/docker.service.d
 cat <<EOF > /etc/systemd/system/docker.service.d/override.conf
 [Service]
@@ -65,8 +71,7 @@ EOF
 cat <<EOF > /etc/docker/daemon.json
 {
   "hosts": [
-    "unix:///var/run/docker.sock",
-    "tcp://$private_ip:2375"
+    "unix:///var/run/docker.sock"
   ],
   "log-driver": "awslogs",
   "log-opts": {
@@ -78,12 +83,9 @@ cat <<EOF > /etc/docker/daemon.json
 }
 EOF
 
+systemctl enable docker.service
 systemctl daemon-reload
 systemctl restart docker.service
-systemctl enable docker.service
-systemctl restart docker.service
-
-usermod -aG docker ubuntu
 
 ##########
 # JOIN SWARM
@@ -93,12 +95,10 @@ retry=0
 until [ $retry -ge 6 ]; do
 
   manager_host=$(aws ssm get-parameters --region "$region" --names "/swarm/$cluster_tag/manager/host" | jq '.Parameters[0].Value // empty' -r)
-  if [ "$manager_host" != '' ]; then
-    join_token=$(aws ssm get-parameters --region "$region" --names "/swarm/$cluster_tag/$role_tag/token" | jq '.Parameters[0].Value // empty' -r)
-    if [ "$join_token" != '' ]; then
-      docker swarm join --token "$join_token" "$manager_host:2377"
-      joined=$?
-    fi
+  join_token=$(aws ssm get-parameters --region "$region" --names "/swarm/$cluster_tag/$role_tag/token" | jq '.Parameters[0].Value // empty' -r)
+  if [ "$manager_host" != '' ] && [ "$join_token" != '' ]; then
+    docker swarm join --token "$join_token" "$manager_host:2377"
+    joined=$?
   fi
 
   if [ $joined == 0 ]; then
@@ -114,57 +114,44 @@ done
 ##########
 # INIT SWARM
 
-if [ "$role_tag" == 'manager' ]; then
-  if [ $joined == 1 ]; then
+if [ "$role_tag" == 'manager' ] && [ $joined == 1 ]; then
 
-    # SWARM
-    docker swarm init
+  # SWARM
+  docker swarm init
 
-    # USER ADMIN
+  # USER ADMIN
+  user_admin_password=$(aws ssm get-parameters --region "$region" --names "/swarm/$cluster_tag/manager/user/admin/password" | jq '.Parameters[0].Value // empty' -r)
+  if [ "$user_admin_password" == '' ]; then
+    user_admin_password=$(openssl rand 256 | sha256sum | head -c 32)
+    aws ssm put-parameter --region "$region" --name "/swarm/$cluster_tag/manager/user/admin/password" --value "$user_admin_password" --type String
     user_admin_password=$(aws ssm get-parameters --region "$region" --names "/swarm/$cluster_tag/manager/user/admin/password" | jq '.Parameters[0].Value // empty' -r)
-    if [ "$user_admin_password" == '' ]; then
-      user_admin_password=$(openssl rand 256 | sha256sum | head -c 32)
-      aws ssm put-parameter --region "$region" --name "/swarm/$cluster_tag/manager/user/admin/password" --value "$user_admin_password" --type String
-      user_admin_password=$(aws ssm get-parameters --region "$region" --names "/swarm/$cluster_tag/manager/user/admin/password" | jq '.Parameters[0].Value // empty' -r)
-    fi
-    user_admin_password_hash=$(docker run --rm httpd:2.4-alpine htpasswd -nbB admin "$user_admin_password" | cut -d ":" -f 2)
-
-    # NETWORK
-    docker network create -d overlay --attachable managers
-
-    # PORTAINER
-    docker service create \
-      --name portainer \
-      --network managers \
-      --constraint=node.role==manager \
-      --publish 8000:8000 --publish 9000:9000 \
-      --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-      portainer/portainer:1.22.1 \
-        --admin-password="$user_admin_password_hash"
-
-    # TRAEFIK
-    docker service create \
-      --name traefik \
-      --network managers \
-      --constraint=node.role==manager \
-      --publish 80:80 --publish 8080:8080 \
-      traefik:v2.0 \
-        --providers.docker.swarmMode=true \
-        --providers.docker.endpoint=tcp://127.0.0.1:2377 \
-        --providers.docker.exposedbydefault=false \
-        --api.insecure=true
-
-    # PROXY
-    docker service create \
-      --name proxy \
-      --network managers \
-      --constraint=node.role==manager \
-      --publish 8888:8888 \
-      --host "docker:$private_ip" \
-      --env BASIC_AUTH_USER=admin \
-      --env BASIC_AUTH_PASS="$user_admin_password" \
-      pierredavidbelanger/basic-auth-forward-proxy
   fi
+  user_admin_password_hash=$(docker run --rm httpd:2.4-alpine htpasswd -nbB admin "$user_admin_password" | cut -d ":" -f 2)
+
+  # NETWORK
+  docker network create -d overlay --attachable managers
+
+  # PORTAINER
+  docker service create \
+    --name portainer \
+    --network managers \
+    --constraint=node.role==manager \
+    --publish 8000:8000 --publish 9000:9000 \
+    --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+    portainer/portainer:1.22.1 \
+      --admin-password="$user_admin_password_hash"
+
+  # TRAEFIK
+  docker service create \
+    --name traefik \
+    --network managers \
+    --constraint=node.role==manager \
+    --publish 80:80 --publish 8080:8080 \
+    --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+    traefik:v2.0 \
+      --providers.docker.swarmMode=true \
+      --providers.docker.exposedbydefault=false \
+      --api.insecure=true
 fi
 
 ##########
@@ -210,4 +197,28 @@ if [ "$role_tag" == 'manager' ]; then
   chmod 644 /home/manager/.ssh/id_rsa.pub
   chmod 600 /home/manager/.ssh/id_rsa
   chmod 600 /home/manager/.ssh/authorized_keys
+fi
+
+##########
+# ROUTE53
+
+cat <<EOF > /tmp/Route53Changes.json
+{
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "manager.$cluster_tag.$zone_tag", "Type": "CNAME", "TTL": 30,
+        "ResourceRecords": [{ "Value": "$public_hostname" }]
+      }
+    }
+  ]
+}
+EOF
+
+if [ "$role_tag" == 'manager' ] && [ "$zone_tag" != "" ]; then
+  zone_id=($(aws route53 list-hosted-zones --query "HostedZones[?Name==\`$zone_tag.\`].Id" --output text))
+  if [ "$zone_id" != "" ]; then
+    aws route53 change-resource-record-sets --hosted-zone-id "$zone_id" --change-batch file:///tmp/Route53Changes.json
+  fi
 fi
